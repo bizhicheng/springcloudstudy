00b5cc59c0560fbe9cb39a0c133ba8bd7ea07c0d
bizhicheng <753033225@qq.com>
2018-06-19 17:54:48 +0800

    填写版本注释信息

---
+++ service-zuul/src/main/java/com/springcloud/servicezuul/ServiceZuulApplication.java
@@ -0,0 +1,19 @@
+package com.springcloud.servicezuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author EnableZuulProx开启zuul的功能
+ */
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class ServiceZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceZuulApplication.class, args);
+    }
+}
